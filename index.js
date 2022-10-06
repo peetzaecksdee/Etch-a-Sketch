31733efa0e3bbd7ce6513da7f70e6d804ba03b6d
peetzaecksdee <peetzaecksdee@hotmail.com>
2022-10-06 16:30:28 +0700

    Finished the main functions, footer and size

--- index.js
+++ index.js
@@ -1,31 +1,89 @@
+let color = "black";
 let columnNumber = 16;
 
-const buttonContainer = document.querySelector(".button-container");
+const squareContainer = document.querySelector(".square-container");
 const sizeText = document.querySelector("#size");
 const sliderSize = document.querySelector("#square-per-columns");
 
-buttonContainer.style.cssText = `grid-template-columns: repeat(${columnNumber}, 1fr); grid-template-columns: repeat(${columnNumber}, 1fr)`;
-sizeText.textContent = `${columnNumber} x ${columnNumber}`;
+function clearGrid() {
+    squareContainer.innerHTML = "";
+    squareContainer.style.cssText = `grid-template-columns: repeat(${columnNumber}, 1fr); grid-template-columns: repeat(${columnNumber}, 1fr)`;
+}
 
-function sizeChanger(total, size) {
-    buttonContainer.replaceChildren();
+function updateSizeText(columnNumber) {
+    sizeText.textContent = `${columnNumber} x ${columnNumber}`;
+}
+
+function setMode( newMode, e ) {
+    activate(e);
+    mode = newMode;
+}
+
+function setupGrid(columnNumber) {
+    clearGrid();
+    updateSizeText(columnNumber);
+
+    const total = columnNumber ** 2;
     for (let i = 0; i < total; i++) {
-        const div = document.createElement("div");
-        div.setAttribute("id", "square");
-        div.style.width = `${size}px`;
-        div.style.height = `${size}px`;
-        buttonContainer.appendChild(div);
+        const square = document.createElement("div");
+        square.setAttribute("id", "square");
+        square.addEventListener("mouseover", changeColor);
+        square.addEventListener("mousedown", changeColor);
+        squareContainer.appendChild(square);
     }
 }
-sliderSize.addEventListener("input", console.log("hello"), false);
-sizeChanger(columnNumber ** 2, 500 / columnNumber);
+setupGrid(columnNumber);
 
-const squares = document.querySelectorAll("#square");
+const colorSel = document.querySelector("#color-selector");
+const colorBtn = document.querySelector("#color");
+const rainbowBtn = document.querySelector("#rainbow");
+const shadingBtn = document.querySelector("#shading");
+const brighteningBtn = document.querySelector("#brightening");
+const eraserBtn = document.querySelector("#eraser");
+const clearBtn = document.querySelector("#clear");
 
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", (e) => {
-        e.target.style.background = "black";
-    });
+let mouseHold = false;
+let mode = "color";
+
+squareContainer.addEventListener("mousedown", () => (mouseHold = true));
+squareContainer.addEventListener("mouseup", () => (mouseHold = false));
+colorSel.addEventListener("input", (e) => color = e.target.value)
+colorBtn.addEventListener("click", (e) => setMode("color", e));
+rainbowBtn.addEventListener("click", (e) => setMode("rainbow", e));
+eraserBtn.addEventListener("click", (e) => setMode("eraser", e));
+clearBtn.addEventListener("click", () => clearCanvas());
+sliderSize.addEventListener("input", (e) => {
+    columnNumber = e.target.value;
+    setupGrid(columnNumber);
 });
 
-function changeBackgroundColor() {}
+document.body.onmousedown = () => (mouseHold = true);
+document.body.onmouseup = () => (mouseHold = false);
+
+function changeColor(e) {
+    if (e.type === "mouseover" && !mouseHold) return;
+    if (mode === "color") {
+        e.target.style.background = color;
+    } else if (mode === "rainbow") {
+        const R = Math.random() * 256;
+        const G = Math.random() * 256;
+        const B = Math.random() * 256;
+        e.target.style.background = `rgb(${R}, ${G}, ${B})`;
+    } else if (mode === "eraser") {
+        e.target.style.background = "";
+    }
+}
+
+function clearCanvas() {
+    const squares = document.querySelectorAll("#square");
+    squares.forEach((square) => {
+        if (square.style.background !== null) square.style.background = "";
+    });
+}
+
+function activate(e) {
+    document.querySelectorAll("button").forEach(button => {
+        if (button.classList.contains("active")) button.classList.remove("active");
+    });
+    e.target.classList.add("active");
+}
